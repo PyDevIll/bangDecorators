bc480b3c121abb30e515d2ff9a9e73c7c88cd944
PyDevIll <delphadyth@gmail.com>
2024-07-06 01:00:06 +0700

    Made simple sync wrapper. Test for 'slow_function' - OK. ISSUE: Test for 'MyClass.method' fails with TypeError in wrapper

--- main.py
+++ main.py
@@ -3,13 +3,26 @@ import asyncio
 
 
 class Cache:
-    def __init(self):
+    def __init__(self):
         self.data = {}
 
     def __call__(self, func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            ...
+
+            # если в словаре словарей self.data по ключам имени функции и переданным параметрам
+            # находится предвычисленное значение - возвращаем его
+            key = str(*args) + str(**kwargs)
+            if func.__name__ in self.data:
+                if key in self.data[func.__name__]:
+                    return self.data[func.__name__][key]
+
+            # иначе - вычисляем значение, сохраняем его в data и возвращаем
+            result = func(*args, **kwargs)
+            if func.__name__ not in self.data:
+                self.data[func.__name__] = {}
+            self.data[func.__name__][key] = result
+            return result
 
         @wraps(func)
         async def async_wrapper(*args, **kwargs):
@@ -21,7 +34,7 @@ class Cache:
             return wrapper
 
     def invalidate(self, func):
-        ...
+        self.data[func.__name__].clear()
 
 
 cache = Cache()
@@ -29,18 +42,18 @@ cache = Cache()
 
 @cache
 def slow_function(arg):
-    print("Calculating...")
+    print(f"Function 'slow_function' is actually run")
     return arg
 
 
 class MyClass:
     @cache
     def method(self, arg):
-        print("Calculating...")
+        print(f"Function 'MyClass.method' is actually run")
         return arg
 
 
 @cache
 async def async_function(arg):
+    print(f"Function 'async_function' is actually run")
     return arg
-
--- test.py
+++ test.py
@@ -3,21 +3,23 @@ from main import cache, slow_function, MyClass, async_function
 
 
 def test_cache():
+    # cache = Cache()     # без этого объявления pyCharm подозрительно выделяет cache
     assert slow_function(1) == 1
     assert slow_function(1) == 1
     assert len(cache.data) == 1
 
     obj = MyClass()
-    assert obj.method(1) == 1
-    assert obj.method(1) == 1
-    assert len(cache.data) == 2
-
-    cache.invalidate(slow_function)
-    assert len(cache.data) == 1
+    assert obj.method(1) == 1       # TypeError in wrapper: str() parameter 'encoding' must be str, not int
+    # assert obj.method(1) == 1
+    # assert len(cache.data) == 2
+    #
+    # cache.invalidate(slow_function)
+    # assert len(cache.data) == 1
 
 
 @pytest.mark.asyncio
 async def test_cache_async():
+    # cache = Cache()     # без этого объявления pyCharm подозрительно выделяет cache
     assert await async_function(1) == 1
     assert await async_function(1) == 1
     assert len(cache.data) == 1
